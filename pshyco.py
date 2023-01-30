dd67051345339a7becb82fa2545a4c8bd4151fea
Pshyco23 <123889845+Pshyco23@users.noreply.github.com>
2023-01-30 13:01:00 +0600

    Create pshyco.py
    git clone 
    https://github.com/Pshyco23/pshyco-basic-setup.git
    cd pshyco-basic setup
    python pshyco.py

---
+++ pshyco.py
@@ -0,0 +1,248 @@
+import os
+
+import sys
+
+os.system("clear")
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100085190331409")
+
+BLACK=("""
+
+\033[1;34m
+
+   
+
+   
+
+  _____     _                     
+
+ |  __ \   | |                    
+
+ | |__) |__| |__  _   _  ___ ___  
+
+ |  ___/ __| '_ \| | | |/ __/ _ \ 
+
+ | |   \__ \ | | | |_| | (_| (_) |
+
+ |_|   |___/_| |_|\__, |\___\___/ 
+
+                   __/ |          
+
+                  |___/           
+
+\033[1;33m××××××××××××××××××××××××××××××××××××××××××××××
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m CREATED BY\033[1;33m     ☞  \033[1;32mpshyco      \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m FACEBOK\033[1;33m        ☞  \033[1;32mpshyco lola \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m GITHUB\033[1;33m         ☞  \033[1;32mlola bot   \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TEAM\033[1;33m           ☞  \033[1;32mpshyco          \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS NAME\033[1;33m     ☞  \033[1;32mPKG INSTALL      \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS VIRSION\033[1;33m  ☞  \033[1;32m0.1             \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS STATUS\033[1;33m   ☞  \033[1;32mWORKING         \033[1;33m   |
+
+××××××××××××××××××××××××××××××××××××××××××××××
+
+""")
+
+print(BLACK)
+
+p=input("ARE YOU RAN THIS TOOL (y/n)  : ")
+
+p=p.replace(" ","")
+
+ys=["y","Y"]
+
+if p in ys:
+
+  print("TOOL IS RUNNING.....")
+
+elif p=="n":
+
+  print("STOP TOOL, PLEASE TRY AGAIN")
+
+  sys.exit()
+
+else:
+
+  print("YOU CHOICE IS WORNG")
+
+  sys.exit()
+
+print("\033[1;32m")
+
+os.system("pkg update -y")
+
+print("\033[1;32m")
+
+os.system("pkg upgrade -y")
+
+print("\033[1;32m")
+
+os.system("pkg install git -y")
+
+print("\033[1;32m")
+
+os.system("pkg install python -y")
+
+print("\033[1;32m")
+
+os.system("pkg install python2 -y")
+
+print("\033[1;32m")
+
+os.system("pkg install wget -y")
+
+print("\033[1;32m")
+
+os.system("pkg install figlet -y")
+
+print("\033[1;32m")
+
+os.system("pkg install curl -y")
+
+print("\033[1;32m")
+
+os.system("pkg install php -y")
+
+print("\033[1;32m")
+
+os.system("pkg install cmatrix -y")
+
+print("\033[1;32m")
+
+os.system("pkg install sl -y")
+
+print("\033[1;32m")
+
+os.system("pkg install ruby -y")
+
+print("\033[1;32m")
+
+os.system("gem install lolcat -y")
+
+print("\033[1;32m")
+
+os.system("pkg install zip -y")
+
+print("\033[1;32m")
+
+os.system("pkg install unzip -y")
+
+print("\033[1;32m")
+
+os.system("pkg install openssh -y")
+
+print("\033[1;32m")
+
+os.system("pkg install clang -y")
+
+print("\033[1;32m")
+
+os.system("pkg install crunch -y")
+
+print("\033[1;32m")
+
+os.system("pkg install proot-distro -y")
+
+print("\033[1;32m")
+
+os.system("pkg install ncurses-utils -y")
+
+print("\033[1;32m")
+
+os.system("python -m pip install --upgrade pip")
+
+print("\033[1;32m")
+
+os.system("python2 -m pip install --upgrade pip")
+
+print("\033[1;32m")
+
+os.system("pip install requests")
+
+print("\033[1;32m")
+
+os.system("pip2 install requests")
+
+print("\033[1;32m")
+
+os.system("pip install rich")
+
+print("\033[1;32m")
+
+os.system("pip2 install rich")
+
+print("\033[1;32m")
+
+os.system("pip install bs4")
+
+print("\033[1;32m")
+
+os.system("pip2 install bs4")
+
+print("\033[1;32m")
+
+os.system("pip install mechanize")
+
+print("\033[1;32m")
+
+os.system("pip2 install mechanize")
+
+print("""
+
+═══════════════════════════════════════════════════
+
+                ✅CONGRATULATION✅
+
+═══════════════════════════════════════════════════
+
+\033[1;33m
+
+                       /$$                        
+
+                      | $$                        
+
+  /$$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$ 
+
+ /$$_____/ /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$
+
+|  $$$$$$ | $$$$$$$$  | $$    | $$  | $$| $$  \ $$
+
+ \____  $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$
+
+ /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/
+
+|_______/  \_______/   \___/   \______/ | $$____/ 
+
+                                        | $$      
+
+                                        | $$      
+
+                                        |__/ 
+
+\033[1;32m
+
+    /$$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$
+
+    | $$__  $$ /$$__  $$| $$$ | $$| $$_____/
+
+    | $$  \ $$| $$  \ $$| $$$$| $$| $$      
+
+    | $$  | $$| $$  | $$| $$ $$ $$| $$$$$   
+
+    | $$  | $$| $$  | $$| $$  $$$$| $$__/   
+
+    | $$  | $$| $$  | $$| $$\  $$$| $$      
+
+    | $$$$$$$/|  $$$$$$/| $$ \  $$| $$$$$$$$
+
+    |_______/  \______/ |__/  \__/|________/
+
+""")
+
